6b6e563ce45ebb9c00eb12a67a6b8d8fef44c683
u6469314 <u6469314@anu.edu.au>
2019-02-06 17:46:58 +1100

    Made changes in Intents

--- app/src/main/java/com/example/hp/login_application/RegistrationActivity.java
+++ app/src/main/java/com/example/hp/login_application/RegistrationActivity.java
@@ -1,13 +1,49 @@
 package com.example.hp.login_application;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RegistrationActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText RegName, RegPassword, RegEmail;
+    private Button RegLogin, RegSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        idAssignment();
+        RegLogin.setOnClickListener(this);
+        RegSignUp.setOnClickListener(this);
+
+    }
+
+    private void idAssignment(){
+        RegName = findViewById(R.id.etRegName);
+        RegEmail = findViewById(R.id.etRegMail);
+        RegPassword = findViewById(R.id.etRegPassword);
+        RegLogin = findViewById(R.id.btnRegLogin);
+        RegSignUp = findViewById(R.id.btnRegSignUp);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnRegLogin:
+                Intent intent = new Intent(RegistrationActivity.this,MainActivity.class);
+                startActivity(intent);
+            case R.id.btnRegSignUp:
+                if (RegName.getText().toString().trim().length() != 0 && RegEmail.getText().toString().trim().length() != 0 && RegPassword.getText().toString().trim().length() != 0){
+                    Intent i = new Intent(RegistrationActivity.this,MainActivity.class);
+                    startActivity(i);
+                }
+
+        }
+
+
     }
 }
